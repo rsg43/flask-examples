8ccb15122092cf0690adf60c2c8f1790a76eb979
rsg43 <ravindag@hotmail.com>
2025-06-05 14:28:13 +0000

    Move logic to a base api file, inherit for examples

--- src/api.py
+++ src/api.py
@@ -6,80 +6,39 @@ specified endpoints, which can be called by clients to interact with the
 service.
 """
 
-from types import TracebackType
-from typing import Union, Callable, Any
-from typing_extensions import Self
+from typing import Callable, Any
 
 from flask import Response
 
-from src.app import WebApp, WebAppConfig
+from src.base_api import BaseWebAPI
 
 
-class WebAPI:
+class WebAPI(BaseWebAPI):
     """
     Base Web API service, which contains the run method to start the web API
     and requires implementations to add endpoint handlers to the web app.
     """
 
     def __init__(self) -> None:
-        self._app = WebApp(
-            config=WebAppConfig(
-                host="0.0.0.0",
-                port=12345,
-                threads=4,
-                connection_limit=100,
-            )
+        super().__init__(
+            host="0.0.0.0",
+            port=12345,
         )
-        self._endpoint_handlers: dict[
-            str, tuple[list[str], str, Callable[..., Any]]
-        ] = {
+
+    @property
+    def _endpoint_handlers(
+        self,
+    ) -> dict[str, tuple[list[str], str, Callable[..., Any]]]:
+        """
+        Property to get the endpoint handlers. This should be implemented by
+        subclasses to provide the actual endpoint handlers.
+
+        :return: The endpoint handlers.
+        :rtype: dict[str, tuple[list[str], str, Callable[..., Any]]]
+        """
+        return {
             "homepage": (["GET"], "/", self._create_homepage),
         }
-
-    def __enter__(self) -> Self:
-        """
-        Method to enter the context manager.
-
-        :return: The web API service.
-        :rtype: Self
-        """
-        return self
-
-    def __exit__(
-        self,
-        exc_type: Union[type[BaseException], None],
-        exc_val: Union[BaseException, None],
-        exc_tb: Union[TracebackType, None],
-    ) -> None:
-        """
-        Method to exit the context manager.
-
-        :param exc_type: The exception type.
-        :type exc_type: Union[type[BaseException], None]
-        :param exc_val: The exception value.
-        :type exc_val: Union[BaseException, None]
-        :param exc_tb: The exception traceback.
-        :type exc_tb: Union[TracebackType, None]
-        """
-
-    def run(self) -> None:
-        """
-        Run the web API. This method adds the endpoint handlers to the web app
-        and starts the server.
-        """
-        for name, (
-            methods,
-            endpoint,
-            handler,
-        ) in self._endpoint_handlers.items():
-            self._app.add_endpoint(
-                endpoint,
-                name,
-                handler,
-                methods,
-            )
-
-        self._app.run()
 
     def _create_homepage(self, params: dict[str, Any]) -> Response:
         """
